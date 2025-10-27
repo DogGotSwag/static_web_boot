287d7888f38c061ba4148196c12458c4351f2d82
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-10-26 20:07:29 -0600

    make multiple test cases for edge cases

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -5,9 +5,33 @@ class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
-
         self.assertEqual(node, node2)
 
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.LINK, "https://www.google.com/")
+        node2 = TextNode("This is a text node", TextType.LINK, "https://www.google.com/")
+        self.assertEqual(node, node2)
 
+    def test_not_eq_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is maybe a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_type(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        node2 = TextNode("This is a text node", TextType.LINK)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_url(self):
+        node = TextNode("This is a text node", TextType.PLAIN, "https://www.google.com/")
+        node2 = TextNode("This is a text node", TextType.LINK)
+        self.assertNotEqual(node, node2)
+
+    def test_all_not_equal(self):
+        node = TextNode("no regular F150", TextType.LINK, "https://www.google.com/")
+        node2 = TextNode("dis a raptor", TextType.PLAIN)
+        self.assertNotEqual(node, node2)
+
+    
 if __name__ == "__main__":
     unittest.main()
