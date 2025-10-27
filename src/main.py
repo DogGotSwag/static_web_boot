c19c2dc2ec01ed8d5cfae0504783fac905dc76a4
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-10-26 19:36:31 -0600

    alter how text_type is set on textnode class
    now it uses the TextType enum in the main.py
    and gets passed into the class

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from textnode import TextNode
+from textnode import TextNode, TextType
 
 def main():
-    obj = TextNode("some anchor text", "LINK", "https://www.boot.dev")
-    objTwo = TextNode("some aanchor text", "LINK", "https://www.boot.dev")
+    obj = TextNode("some anchor text", TextType.LINK, "https://www.boot.dev")
+    objTwo = TextNode("some aanchor text", TextType.LINK, "https://www.boot.dev")
     print(obj)
     print(obj == objTwo)
 
