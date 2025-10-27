b68853687aa80ff2ebc1896a5c33aeb02a44c18d
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-10-26 19:38:15 -0600

    add bold type and change names of types

--- src/textnode.py
+++ src/textnode.py
@@ -1,16 +1,17 @@
 from enum import Enum
     
 class TextType(Enum):
-    PLAIN_TEXT = "plain",
-    ITALIC_TEXT = "italic",
-    CODE_TEXT = "code",
+    PLAIN = "plain",
+    BOLD = "bold",
+    ITALIC = "italic",
+    CODE = "code",
     LINK = "link",
     IMAGE = "image"
 
 class TextNode():
     def __init__(self, text, text_type, url = None):
         self.text = text
-        self.text_type = TextType[text_type]
+        self.text_type = text_type
         self.url = url
 
     def __eq__(self, other):
