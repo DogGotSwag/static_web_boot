215bb602976e46a48176830e82fcd5c5fe8cf065
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-10-26 19:26:18 -0600

    test TextNode class inside main.py
    testing the __repr__ and __eq__ specifically

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from textnode import TextNode
+
+def main():
+    obj = TextNode("some anchor text", "LINK", "https://www.boot.dev")
+    objTwo = TextNode("some aanchor text", "LINK", "https://www.boot.dev")
+    print(obj)
+    print(obj == objTwo)
+
+main()
