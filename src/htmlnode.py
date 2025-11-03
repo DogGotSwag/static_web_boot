b419bf97fd6e9d1bab1d9ee64dfde0850d495eaa
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-11-02 19:21:25 -0700

    add a __repr__ to htmlnode class
    will make debugging easier

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -14,7 +14,21 @@ class HTMLNode():
             string += f' {key}="{self.props[key]}"'
         return string
     
+    def __repr__(self):
+        return f"""
+        tag: {self.tag}
+        value: {self.value}
+        children: {self.children}
+        props: {self.props}
+        """
+        
+    
 dis_dict = {
     "href": "https://www.google.com",
     "target": "_blank",
 }
+
+
+test = HTMLNode("a", "hello fn", "h2", dis_dict)
+print(test)
+
