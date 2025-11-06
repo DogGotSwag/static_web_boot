e588ef3863b429de775466371d32c36113fe55ed
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-11-06 00:57:45 -0700

    refactor text_node_to_html_node function
    converted to a switch statement because it was simpler to read
    and understand and i made the default case raise the Exception

--- src/text_node_to_html_node.py
+++ src/text_node_to_html_node.py
@@ -2,21 +2,17 @@ from textnode import TextNode, TextType
 from htmlnode import LeafNode
 
 def text_node_to_html_node(text_node):
-    if text_node.text_type not in TextType:
-        raise Exception("wrong type")
-    
-    if text_node.text_type is TextType.TEXT:
-        return LeafNode(None, text_node.text)
-    
-    if text_node.text_type is TextType.BOLD:
-        return LeafNode("b", text_node.text)
-    
-    if text_node.text_type is TextType.ITALIC:
-        return LeafNode("i", text_node.text)
-    
-    if text_node.text_type is TextType.LINK:
-        return LeafNode("a", text_node.text, {"href": ""})
-    
-    if text_node.text_type is TextType.IMAGE:
-        return LeafNode("i", text_node.text, {"src": "", "alt" : ""})
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": ""})
+        case TextType.IMAGE:
+            return LeafNode("i", text_node.text, {"src": "", "alt" : ""})
+        case _:
+            raise Exception("wrong type")
 
