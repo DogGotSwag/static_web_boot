595ecc0c8cfd6b9852131f36d638ca6ec298ee80
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-11-05 19:09:12 -0700

    create text_node_to_html_node function
    converts a text node into a html node specifically
    a leaf node

---
+++ src/text_node_to_html_node.py
@@ -0,0 +1,22 @@
+from textnode import TextNode, TextType
+from leafnode import LeafNode
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type not in TextType:
+        raise Exception("wrong type")
+    
+    if text_node.text_type is TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    
+    if text_node.text_type is TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    
+    if text_node.text_type is TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    
+    if text_node.text_type is TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": ""})
+    
+    if text_node.text_type is TextType.IMAGE:
+        return LeafNode("i", text_node.text, {"src": "", "alt" : ""})
+
