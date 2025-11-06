8f43b736bcbe233bca6297501d18e520c62b2b4c
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-11-05 19:24:55 -0700

    move leafnode/parentnode to htmlnode.py
    it makes finding the related classes much easier and having
    less files is always good

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -22,3 +22,31 @@ class HTMLNode():
         props: {self.props}
         """
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None or self.value == "":
+            raise ValueError("All leaf nodes must have a value")
+        if self.tag is None:
+            return self.value
+        if self.props is not None:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None or self.tag == "":
+            raise ValueError("has no tag")
+        if self.children is None:
+            raise ValueError("children value is missing")
+
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}>{children_html}</{self.tag}>"
+    
