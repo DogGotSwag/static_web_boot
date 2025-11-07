c02088c9ba1a4e85083e8415a7f07df6a6d2826d
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-11-07 04:00:56 -0700

    create split_nodes_delimiter
    new function creates TextNodes from raw markdown

---
+++ src/inline_markdown.py
@@ -0,0 +1,18 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    all_results = []
+    for old_node in old_nodes:
+        if old_node.text_type is not TextType.TEXT:
+            all_results.append(old_node)
+        else:
+            split_result = old_node.text.split(delimiter)
+            if len(split_result) == 1:
+                raise ValueError("invalid Markdown syntax")
+            result = [
+                TextNode(split_result[0], TextType.TEXT),
+                TextNode(split_result[1], text_type),
+                TextNode(split_result[2], TextType.TEXT),
+            ]
+            all_results.extend(result)
+    return all_results
