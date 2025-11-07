782f804e6836c800c255b4173aeca888bb9bc9f5
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-11-07 05:29:13 -0700

    fix extract_markdown_links to not match images
    using a negative look behind (?<!patter_here)

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -22,4 +22,10 @@ def extract_markdown_images(text):
     return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
 
 def extract_markdown_links(text):
-    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
+    return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
+
+# def split_nodes_image(old_nodes):
+#     pass
+
+# def split_nodes_link(old_nodes):
+#     pass
