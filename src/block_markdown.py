91fb842539e72449b093c48072de323dcc705df4
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-11-13 01:35:41 -0700

    make enum for block types
    will be used to determine what type of block
    a piece of markdown text is

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class blockType(Enum):
+    PARAGRAPH = "paragraph", 
+    HEADING = "heading",
+    CODE = "code",
+    QUOTE = "quote",
+    UNORDERED = "unordered_list",
+    ORDERED = "ordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split('\n\n')
     final_list = []
