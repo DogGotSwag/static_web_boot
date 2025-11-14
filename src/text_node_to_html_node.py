45c8d712e5ff2ca0b37a64783eeadca551f14c19
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-11-14 00:49:49 -0700

    add code type to text_node_to_html_node

--- src/text_node_to_html_node.py
+++ src/text_node_to_html_node.py
@@ -13,6 +13,8 @@ def text_node_to_html_node(text_node):
             return LeafNode("a", text_node.text, {"href": text_node.url})
         case TextType.IMAGE:
             return LeafNode("img", "", {"src": text_node.url, "alt" : text_node.text})
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
         case _:
             raise Exception("wrong type")
 
