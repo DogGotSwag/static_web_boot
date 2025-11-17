c7a0ca7dd75845d5b27c67c864378c9e81e0b769
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-11-16 19:00:34 -0700

    complete copy_files_and_dir_recursive
    this copies contents of one dir to another

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
-def main():
-    obj = TextNode("some anchor text", TextType.LINK, "https://www.boot.dev")
-    objTwo = TextNode("some aanchor text", TextType.LINK, "https://www.boot.dev")
-    print(obj)
-    print(obj == objTwo)
+def copy_files_and_dir_recursive(source, destination):
+    files_and_dirs = os.listdir(source)
+    for file_or_dir in files_and_dirs:
+        curr = os.path.join(source, file_or_dir)
+        if os.path.isfile(curr):
+            shutil.copy(curr, destination)
+        else:
+            path = os.path.join(destination, file_or_dir)
+            os.mkdir(path)
+            copy_files_and_dir_recursive(curr,path)
 
-main()
+def src_dir_to_des_dir():
+    destination = './public'
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    os.mkdir(destination)
+    copy_files_and_dir_recursive('./static', './public')
+
+src_dir_to_des_dir()
